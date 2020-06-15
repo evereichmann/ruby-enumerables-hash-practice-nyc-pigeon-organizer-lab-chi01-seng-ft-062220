8b74ca9d10f24963962e764ecdce208d127e5bad
Eve Reichmann <ereichmann4@aol.com>
2020-06-15 21:47:30 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,15 +1,33 @@
+#my independent solution
+require 'pry'
 def nyc_pigeon_organizer(data)
-  data.each_with_object({}) do |(key, value), final_array|
+pigeon_name = Hash.new 
+  data.each do |key, value|
     value.each do |inner_key, names|
       names.each do |name|
-        if !final_array[name]
-          final_array[name] = {}
-          end
-          if !final_array[name][key]
-          !final_array[name][key] = []
-        end
-      final_array[name][key].push(inner_key.to_s)
-      end
+        if pigeon_name.include? name
+          
+        else 
+binding.pry
+     end 
     end
   end
-end
+  pigeon_name
+end  
+
+# used the video to get through lab 
+#def nyc_pigeon_organizer(data)
+# data.each_with_object({}) do |(key, value), final_array|
+#   value.each do |inner_key, names|
+#     names.each do |name|
+#        if !final_array[name]
+#          final_array[name] = {}
+#          end
+#          if !final_array[name][key]
+#          !final_array[name][key] = []
+#        end
+#      final_array[name][key].push(inner_key.to_s)
+#      end
+#    end
+# end
+#end
